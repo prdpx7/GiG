135df5000017f1e0f7a8568773739cd76165e75f
Pradeep Khileri <pradeepchoudhary009@gmail.com>
2016-10-19 01:22:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if [ ! -d $HOME/.GiG ]; then
     git clone https://github.com/zuck007/GiG.git ~/.GiG
     echo "Copying script into the /usr/local/bin/ dir"
-    sudo cp gig /usr/local/bin/
+    sudo cp ~/.GiG/gig /usr/local/bin/
     
 else
     cd ~/.GiG && git pull origin master && cd -
