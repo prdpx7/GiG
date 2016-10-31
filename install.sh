b8dcf8e34b4435792aef49740ba0757ca73a0874
zuck007 <pradeepchoudhary009@gmail.com>
2016-11-01 00:03:58 +0530

    added update option

--- install.sh
+++ install.sh
@@ -2,12 +2,11 @@
 
 if [ ! -d $HOME/.GiG ]; then
     git clone https://github.com/zuck007/GiG.git ~/.GiG
-    echo "Copying script into the /usr/local/bin/ dir"
-    sudo cp ~/.GiG/gig /usr/local/bin/
     
 else
     cd ~/.GiG && git pull origin master && cd -
     echo "Updated GiG!!!"
 fi
 
-
+echo "Copying gig script into the /usr/local/bin/ directory"
+sudo cp ~/.GiG/gig /usr/local/bin/
